54c0ef7749adadb6f77ef5ba4eaf87b237d0bd30
Tucker <public.tuckersn@gmail.com>
2022-05-11 23:36:35 -0700

    Setting up React app layout

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -8,14 +8,36 @@ import {
 	Link,
 	Outlet
   } from "react-router-dom";
+import styled from 'styled-components';
+
+
+const Frame = styled.div`
+	height: 100%;
+	width: 100%;
+	display: flex;
+`;
+
+const SideBar = styled.div`
+	flex: 0 0 200px;
+	border-right: 2px solid rga(0,0,0,0.1);
+`;
+
+const Content = styled.div`
+	flex: 1;
+`;
+
 
 function App() {
 	return (
-		<div>
-			app stuff here
-			<Link to={'/me'}>test</Link>
-			<Outlet></Outlet>
-		</div>
+		<Frame>
+			<SideBar>
+				app stuff here
+				<Link to={'/me'}>test</Link>
+			</SideBar>
+			<Content>
+				<Outlet></Outlet>
+			</Content>
+		</Frame>
 	);
 }
 
