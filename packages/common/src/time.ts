8ad27b38359add5225cab00e277e1db4f37c06b7
AvaAvarai <alice.williams.tech@gmail.com>
2022-06-19 04:33:04 -0700

    Fix month indexing to match date-fns 0th starting

--- packages/common/src/time.ts
+++ packages/common/src/time.ts
@@ -5,61 +5,6 @@ export interface DateRange {
 	end: Date;
 }
 
-
-export enum DayOfWeek {
-	SUNDAY = 0,
-	MONDAY = 1,
-	TUESDAY = 2,
-	WEDNESDAY = 3,
-	THURSDAY = 4,
-	FRIDAY = 5,
-	SATURDAY = 6
-}
-
-export function numericDayOfWeek(dayOfWeek: string): number {
-	dayOfWeek = dayOfWeek.toLowerCase();
-	dayOfWeek = dayOfWeek.trim();
-	switch (dayOfWeek) {
-		case "sunday":
-			return DayOfWeek.SUNDAY;
-		case "monday":
-			return DayOfWeek.MONDAY;
-		case "tuesday":
-			return DayOfWeek.TUESDAY;
-		case "wednesday":
-			return DayOfWeek.WEDNESDAY;
-		case "thursday":
-			return DayOfWeek.THURSDAY;
-		case "friday":
-			return DayOfWeek.FRIDAY;
-		case "saturday":
-			return DayOfWeek.SATURDAY;
-		default:
-			throw new Error("Invalid day of week");
-	}
-}
-
-export function dayOfWeekToString(dayOfWeek: DayOfWeek): string {
-	switch (dayOfWeek) {
-		case DayOfWeek.SUNDAY:
-			return "Sunday";
-		case DayOfWeek.MONDAY:
-			return "Monday";
-		case DayOfWeek.TUESDAY:
-			return "Tuesday";
-		case DayOfWeek.WEDNESDAY:
-			return "Wednesday";
-		case DayOfWeek.THURSDAY:
-			return "Thursday";
-		case DayOfWeek.FRIDAY:
-			return "Friday";
-		case DayOfWeek.SATURDAY:
-			return "Saturday";
-		default:
-			throw new Error("Invalid day of week");
-	}
-}
-
 export function getHoursAndMinutes(date: Date): number {
 	return getHours(date) + (date.getMinutes() / 60);
 }
@@ -79,14 +24,14 @@ export function endOfDate(date: Date): Date {
 export function getWeekRangeOfDate(date: Date): DateRange {
 	return {
 		start: startOfDate(subDays(date, date.getDay())),
-		end: endOfDate(addDays(date, 6 - date.getDay()))
+		end: endOfDate(addDays(date, 7 - date.getDay()))
 	}
 }
 
 export function getMonthRangeOfDate(date: Date): DateRange {
 	return {
-		start: startOfDate(new Date(date.getFullYear(), date.getMonth(), 1)),
-		end: endOfDate(subDays(new Date(date.getFullYear(), date.getMonth() + 1), 1))
+		start: startOfDate(new Date(date.getFullYear(), date.getMonth()-1, 1)),
+		end: endOfDate(subDays(new Date(date.getFullYear(), date.getMonth()), 1))
 	}
 }
 
--- packages/common/tests/time.test.ts
+++ packages/common/tests/time.test.ts
@@ -1,17 +1,16 @@
-import { addDays, sub, subDays } from "date-fns";
+import { addDays, subDays } from "date-fns";
 import { getWeekRangeOfDate, getMonthRangeOfDate, endOfDate } from "../src/time";
+// TODO: test month ranges function
+const janFirstTwoThousand = new Date(2020, 0, 1); // Week range of Jan 1st 2020
 
-const janFirstTwoThousand = new Date(2020, 0, 1);
-
-// https://mochajs.org/#getting-started
 var assert = require('assert');
-describe('Week range of Jan 1st 2020', function () {
-	describe('#indexOf()', function () {
-		it('Should return 3 days before, and 4 days after.', function () {
-			assert.deepEqual(getWeekRangeOfDate(janFirstTwoThousand), {
-				start: subDays(janFirstTwoThousand, 3),
-				end: endOfDate(addDays(janFirstTwoThousand, 3))
-			});
-		});
-	});
+describe('Verify week ranges accuracy', function () {
+    describe('getWeekRangeOfDate()', function () {
+        it('Should return 3 days before, and 4 days after.', function () {
+            assert.deepEqual(getWeekRangeOfDate(janFirstTwoThousand), {
+                start: subDays(janFirstTwoThousand, 3),
+                end: endOfDate(addDays(janFirstTwoThousand, 4))
+            });
+        });
+    });
 });
