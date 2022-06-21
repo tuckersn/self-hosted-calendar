3cde9977b4bb7fb7807abc7aaec415a6cad0099a
AvaAvarai <alice.williams.tech@gmail.com>
2022-06-21 15:54:57 -0700

    getWeekRangeOfDate function update and constants for week end/start

--- packages/common/src/time.ts
+++ packages/common/src/time.ts
@@ -1,29 +1,31 @@
-import { Interval, addDays, getHours, setHours, setMilliseconds, setMinutes, setSeconds, subDays } from "date-fns";
+import { Interval, addDays, getHours, setDay, setHours, setMilliseconds, setMinutes, setSeconds, subDays } from "date-fns"
+
+const START_OF_WEEK = 0
+const END_OF_WEEK = 6
 
 export function getHoursAndMinutes(date: Date): number {
-    return getHours(date) + (date.getMinutes() / 60);
+    return date.getHours() + date.getMinutes() / 60
 }
 
 export function getHoursAndMinutesAndSeconds(date: Date): number {
-    return getHours(date) + (date.getMinutes() / 60) + (date.getSeconds() / 3600);
+    return date.getHours() + date.getMinutes() / 60 + date.getSeconds() / 3600
 }
 
 export function startOfDate(date: Date): Date {
-    return setMilliseconds(setSeconds(setMinutes(setHours(date, 0), 0), 0), 0);
+    return setMilliseconds(setSeconds(setMinutes(setHours(date, 0), 0), 0), 0)
 }
 
 export function endOfDate(date: Date): Date {
-    return setMilliseconds(setSeconds(setMinutes(setHours(date, 23), 59), 59), 999);
+    return setMilliseconds(setSeconds(setMinutes(setHours(date, 23), 59), 59), 999)
 }
 
 export function getWeekRangeOfDate(date: Date): Interval {
     return {
-        start: startOfDate(new Date(date.getFullYear(), date.getMonth(), 0)),
-        end: endOfDate(addDays(date, date.getDay()))
+        start: startOfDate(new Date(date.setDate(date.getDate() - date.getDay() - START_OF_WEEK))),
+        end: endOfDate(new Date(date.setDate(date.getDate() - date.getDay() + END_OF_WEEK)))
     }
 }
 
-//TODO: Consider varying days in each month via date-fns package.
 export function getMonthRangeOfDate(date: Date): Interval {
     return {
         start: startOfDate(new Date(date.getFullYear(), date.getMonth()-1, date.getDay())),
--- packages/common/tests/time.test.ts
+++ packages/common/tests/time.test.ts
@@ -1,29 +1,21 @@
-import { addDays, subDays } from "date-fns";
-import { getWeekRangeOfDate, getMonthRangeOfDate, endOfDate } from "../src/time";
+import { getWeekRangeOfDate, getMonthRangeOfDate, endOfDate } from "../src/time"
 
-// TODO: individually test each function
-// TODO: add to getWeekRangeOfDate test set
-
-/* Date notes:              YYYY-MM-DD
- * new Date(2020, 0, 0) === 2019-12-31
- * new Date(2020, 1, 1) === 2020-02-01
- * new Date(2020, 0, 1) === 2020-01-01
- */
+//TODO: Add more test cases, test other functions from ../src/time.ts
 
 const tests = {
-    test1: {start: new Date(2022, 5, 21), interval: {start: new Date(2022, 5, 29), end: new Date(2022, 5, 4)}},
+    test1: {start: new Date(2022, 5, 21), interval: {start: new Date(2022, 5, 19), end: new Date(2022, 5, 25, 23, 59, 59, 999)}},
     // more tests for getWeekRangeOfData function go here
-};
+}
 
 var assert = require('assert');
 describe('Verify week ranges accuracy', function () {
     describe('getWeekRangeOfDate()', function () {
         for (const key in tests) {
-            const value = tests[key as keyof typeof tests];
+            const value = tests[key as keyof typeof tests]
             it (key, function () {
-                assert.deepEqual(getWeekRangeOfDate(value.start), value.interval);
-            });
-        };
-    });
-});
+                assert.deepEqual(getWeekRangeOfDate(value.start), value.interval)
+            })
+        }
+    })
+})
 
